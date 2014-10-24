ea46065a88b5a13fdcd443830b6ebe9124a2cb73
robert <rpankrath@babbel.com>
2014-10-24 11:47:10 +0200

    added clouds

--- AngryWords/AngryWords/GameScene.swift
+++ AngryWords/AngryWords/GameScene.swift
@@ -16,10 +16,36 @@ class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
        
+        self.backgroundColor = AngyWordsStyleKit.babbelBlue
+        
         terrain.createTerrain(-40, length: self.size.width+80, miny: 50, maxY: 500, initialFlatLength: 200, endFlatRange: 500)
         terrain.position = CGPoint(x: 0, y: 0)
+        terrain.zPosition = 1
+        createClouds()
         self.addChild(terrain)
         createTarget()
+        createSling()
+    }
+    
+    func createClouds(){
+        
+        for var x=50; x<Int(self.size.width); x+=200{
+            var y = (random()%Int(self.size.height/2)) + Int(self.size.height/2)
+            var texture = SKTexture(image: AngyWordsStyleKit.imageOfCanvasCloud)
+            var cloud = SKSpriteNode(texture: texture)
+            cloud.xScale = 0.6
+            cloud.yScale = 0.6
+            cloud.position = CGPoint(x: x, y: y)
+            cloud.zPosition = 0
+            var dx = CGFloat(random()%40)
+            var dur = NSTimeInterval(random()%5)+5
+            var move1 = SKAction.moveByX(dx, y: 0, duration: dur)
+            var move2 = SKAction.moveByX(-dx, y: 0, duration: dur)
+            var action = SKAction.repeatActionForever(SKAction.sequence([ move1, move2]))
+            cloud.runAction(action)
+            self.addChild(cloud)
+        }
+        
     }
     
     func createTarget(){
@@ -46,7 +72,13 @@ class GameScene: SKScene {
         node.name = "block"
         node.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: w, height: h))
         node.position = pos
+        node.zPosition = 10
         self.addChild(node)
+    }
+    
+    func createSling(){
+        var xpos = terrain.startAreaXRange.location + terrain.startAreaXRange.length/2
+        
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
