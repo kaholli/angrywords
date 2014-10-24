6d3ca17bc1ccced84d63cba489ff7592961b0504
robert <rpankrath@babbel.com>
2014-10-24 11:20:30 +0200

    first terrain, blocks

--- AngryWords/AngryWords/GameScene.swift
+++ AngryWords/AngryWords/GameScene.swift
@@ -9,37 +9,54 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var terrain : TerrainNode = TerrainNode()
+
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+       
+        terrain.createTerrain(-40, length: self.size.width+80, miny: 50, maxY: 500, initialFlatLength: 200, endFlatRange: 500)
+        terrain.position = CGPoint(x: 0, y: 0)
+        self.addChild(terrain)
+        createTarget()
+    }
+    
+    func createTarget(){
         
-        self.addChild(myLabel)
+        var xRange = terrain.targetAreaXRange
+        let baseY = terrain.targetAreaY
+        
+        var blockLength = CGFloat(80)
+        NSLog("xRange:%i %i", xRange.location, xRange.length)
+        for var i=CGFloat(xRange.location + 10); i<CGFloat(xRange.location + xRange.length); i+=blockLength {
+            createBlock(CGPoint(x: i, y: baseY+blockLength/2), length: blockLength, vertical: true)
+        }
+        
+        for var i=CGFloat(xRange.location + 10); i<CGFloat(xRange.location + xRange.length); i+=blockLength {
+            createBlock(CGPoint(x: i, y: baseY+blockLength), length: blockLength, vertical: false)
+        }
+        
+    }
+    
+    func createBlock(pos: CGPoint, length:CGFloat, vertical:Bool){
+        var w = vertical ? 10 : length;
+        var h = vertical ? length : 10;
+        var node = SKSpriteNode(color: SKColor(red: 0.3, green:0, blue: 0.4, alpha: 1), size: CGSize(width: w, height: h));
+        node.name = "block"
+        node.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: w, height: h))
+        node.position = pos
+        self.addChild(node)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+      
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    
 }
---
+++ AngryWords/AngryWords/TerrainNode.swift
@@ -0,0 +1,67 @@
+//
+//  TerrainNode.swift
+//  AngryWords
+//
+//  Created by Robert Pankrath on 24.10.14.
+//  Copyright (c) 2014 Lesson Nine GmbH. All rights reserved.
+//
+
+import SpriteKit
+
+class TerrainNode: SKShapeNode {
+
+    var targetAreaY = CGFloat(0)
+    var startAreaY = CGFloat(0)
+    var targetAreaXRange = NSMakeRange(0, 0)
+    var startAreaXRange = NSMakeRange(0, 0)
+
+    func createTerrain(fromX: CGFloat, length: CGFloat , miny:CGFloat, maxY:CGFloat, initialFlatLength: CGFloat , endFlatRange: CGFloat){
+        
+        var path = UIBezierPath()
+        
+        var x = fromX;
+        var y = randomBetween(miny, max: maxY)
+        
+        startAreaY = y;
+        path.moveToPoint(CGPoint(x: x, y: y))
+        
+        startAreaXRange = NSMakeRange(Int(x), Int(initialFlatLength))
+
+        x += initialFlatLength;
+        path.addLineToPoint(CGPoint(x: x , y: y))
+        
+        x  = length  - endFlatRange;
+        
+        y = randomBetween(miny, max: maxY)
+        targetAreaY = y
+        path.addQuadCurveToPoint(CGPoint(x: x, y: targetAreaY), controlPoint: CGPoint(x:x/2,y:randomBetween(miny, max: maxY)))
+        
+        targetAreaXRange = NSMakeRange(Int(x), Int(endFlatRange))
+        
+        x += endFlatRange
+        
+        path.addLineToPoint(CGPoint(x: x, y:y))
+        
+        
+        path.addLineToPoint(CGPoint(x: x, y: -100))
+        path.addLineToPoint(CGPoint(x: fromX, y: -100))
+        path.closePath()
+        
+        self.path = path.CGPath
+        
+        
+        self.fillColor = SKColor(red: 0.9, green: 0.87, blue: 0.6, alpha: 1)
+        self.strokeColor = SKColor(red: 0.5, green: 0.45, blue: 0.1, alpha: 1)
+//        self.lineWidth = 20
+        self.physicsBody = SKPhysicsBody(edgeChainFromPath: path.CGPath)
+    }
+    
+    func randomBetween(min: CGFloat, max:CGFloat)->CGFloat{
+        
+        let mi = Int(min);
+        let ma = Int(max);
+        let result = random()%(ma-mi);
+        return CGFloat(mi + result);
+    }
+    
+}
